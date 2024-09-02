5cd95ca53d5e8dcdf20b29e63b345183d52a3f9f
noetipo <noe.tipo@gmail.com>
2024-09-02 09:03:09 -0500

    gateway-server

--- ms-cliente-service/src/main/java/com/example/msclienteservice/repository/ClienteRepository.java
+++ ms-cliente-service/src/main/java/com/example/msclienteservice/repository/ClienteRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-    List<Cliente> findByCode(String code);
+
 }
