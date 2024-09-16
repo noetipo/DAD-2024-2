443b6053aaef757ae5d954377ae558358c05c575
noetipo <noe.tipo@gmail.com>
2024-09-16 08:35:30 -0500

    patron circuitbreaker

--- ms-pedido/src/main/java/com/example/mspedido/feign/ClientFeign.java
+++ ms-pedido/src/main/java/com/example/mspedido/feign/ClientFeign.java
@@ -1,6 +1,7 @@
 package com.example.mspedido.feign;
 
 import com.example.mspedido.dto.ClientDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,5 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "ms-cliente-service", path = "/cliente")
 public interface ClientFeign {
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "clientListByIdCB", fallbackMethod = "clientListById")
+
     public ResponseEntity<ClientDto> getById(@PathVariable Integer id);
+
+    default ResponseEntity<ClientDto> clientListById(Integer id, Exception e) {
+        return ResponseEntity.ok(new ClientDto());
+    }
 }
--- ms-pedido/src/main/java/com/example/mspedido/feign/ProductFeign.java
+++ ms-pedido/src/main/java/com/example/mspedido/feign/ProductFeign.java
@@ -1,6 +1,7 @@
 package com.example.mspedido.feign;
 
 import com.example.mspedido.dto.ProductDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,5 +12,10 @@ import java.util.Optional;
 @FeignClient(name = "ms-catalogo-service", path = "/product")
 public interface ProductFeign {
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "productListByIdCB", fallbackMethod = "productListById")
     public ResponseEntity<ProductDto> getById(@PathVariable Integer id);
+    default ResponseEntity<ProductDto> productListById(Integer id, Exception e) {
+        return ResponseEntity.ok(new ProductDto());
+    }
+
 }
