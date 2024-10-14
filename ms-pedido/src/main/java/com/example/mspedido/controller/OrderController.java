335f64d468a4957c9b4142b06fc2b1199f63a0f0
noetipo <noe.tipo@gmail.com>
2024-10-14 09:52:31 -0500

    data

--- ms-pedido/src/main/java/com/example/mspedido/controller/OrderController.java
+++ ms-pedido/src/main/java/com/example/mspedido/controller/OrderController.java
@@ -1,8 +1,15 @@
 package com.example.mspedido.controller;
 
+import com.example.mspedido.dto.ClientDto;
+import com.example.mspedido.dto.ErrorResponseDto;
+import com.example.mspedido.dto.ProductDto;
 import com.example.mspedido.entity.Order;
+import com.example.mspedido.entity.OrderDetail;
+import com.example.mspedido.feign.ClientFeign;
+import com.example.mspedido.feign.ProductFeign;
 import com.example.mspedido.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +22,11 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private ClientFeign clientFeign;
+    @Autowired
+    private ProductFeign productFeign;
+
     @GetMapping
     public ResponseEntity<List<Order>> getAll() {
         return ResponseEntity.ok(orderService.list());
@@ -26,8 +38,23 @@ public class OrderController {
     }
 
     @PostMapping
-    public ResponseEntity<Order> create(@RequestBody Order order) {
-        return ResponseEntity.ok(orderService.save(order));
+    public ResponseEntity<?> create(@RequestBody Order order) {
+        ClientDto clientDto = clientFeign.getById(order.getClientId()).getBody();
+
+        if (clientDto == null || clientDto.getId() == null) {
+            String errorMessage = "Error: Cliente no encontrado.";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDto(errorMessage));
+        }
+        for (OrderDetail orderDetail : order.getOrderDetails()) {
+            ProductDto productDto = productFeign.getById(orderDetail.getProductId()).getBody();
+
+            if (productDto == null || productDto.getId() == null) {
+                String errorMessage = "Error: producto no encontrado.";
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDto(errorMessage));
+            }
+        }
+        Order newOrder = orderService.save(order);
+        return ResponseEntity.ok(newOrder);
     }
 
     @PutMapping("/{id}")
