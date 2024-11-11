a5c013a22ecfe9fbbb5d6b4ba4c181482edaff41
noetipo <noe.tipo@gmail.com>
2024-11-11 09:00:07 -0500

    cors

--- ms-catalogo/src/main/java/com/example/mscatalogo/controller/ProductController.java
+++ ms-catalogo/src/main/java/com/example/mscatalogo/controller/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/product")
+    @RequestMapping("/product")
 public class ProductController {
     @Autowired
     private ProductService productService;
